f76446415cc3421235a06dd647dc8102dae474fa
Artyom Nikiforov <dom2411edinstvo@gmail.com>
2024-10-07 21:31:38 +0500

    07/10/24

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,16 @@
-let inp = document.querySelector('#tel');
-function noDigits(event) {
-  if ("1234567890".indexOf(event.key) != -1)
-    event.preventDefault();
-}
+let inp = document.getElementById('#phone');
 
-// // Проверяем фокус
-// inp.addEventListener('focus', _ => {
-//   // Если там ничего нет или есть, но левое
-//   if(!/^\+\d*$/.test(inp.value))
-//     // То вставляем знак плюса как значение
-//     inp.value = '+';
-// });
 
-// inp.addEventListener('keypress', e => {
-//   // Отменяем ввод не цифр
-//   if(!/\d/.test(e.key))
-//     e.preventDefault();
-// });
-// <form action="">
-//   {/* Ограничение на длину: maxlength */}
-//   <input type="text" name="tel" placeholder="Номер телефона" required maxlength="13" id="tel">
-// </form>
+// Проверяем фокус
+inp.addEventListener('focus', _ => {
+  // Если там ничего нет или есть, но левое
+  if(!/^\+\d*$/.test(inp.value))
+    // То вставляем знак плюса как значение
+    inp.value = '+7';
+});
+
+inp.addEventListener('keypress', e => {
+  // Отменяем ввод не цифр
+  if(!/\d/.test(e.key))
+    e.preventDefault();
+});
